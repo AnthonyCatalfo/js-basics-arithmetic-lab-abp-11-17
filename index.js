450047bfe236ea52aebed0c0cec45baa5a31aca4
AnthonyCatalfo <anthonycatalfo@gmail.com>
2017-11-12 10:46:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ newID=oldID+1000000000;
 ageIsValid=Number.isInteger(currentAge);
 randomNumber=Math.random() * 20;
 randomInteger=Math.floor(randomNumber);
-randomUserID=randomInteger++1000000001;
+randomUserID=randomInteger+1000000001;
